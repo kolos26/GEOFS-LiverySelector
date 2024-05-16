b6582c152813068f796ebdee97c7d943a864b4f5
scitor <scitor@el337.de>
2024-05-16 12:49:59 +0200

    more templating, tested in browser, fully working now

--- main.js
+++ main.js
@@ -7,10 +7,13 @@ let origHTMLs = {};
 (function init() {
 
     // styles
-    appendChild(document.body, 'div').innerHTML = generateStylesHTML();
+    document.head.appendChild(generateStylesHTML());
+    appendNewChild(document.head, 'link',
+        {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'}
+    );
 
     // Panel for list
-    const listDiv = appendChild(document.querySelector('.geofs-ui-left'), 'div', {
+    const listDiv = appendNewChild(document.querySelector('.geofs-ui-left'), 'div', {
         id: 'listDiv',
         class: 'geofs-list geofs-toggle-panel geofs-livery-list geofs-visible',
         'data-noblur': 'true',
@@ -21,8 +24,8 @@ let origHTMLs = {};
 
     // Button for panel
     const geofsUiButton = document.querySelector('.geofs-ui-bottom');
-    const liveryButton = appendChild(geofsUiButton, 'div', {}, geofs.version >= 3.6 ? 4 : 3);
-    liveryButton.innerHTML = generateButtonPanelHTML();
+    const insertPos = geofs.version >= 3.6 ? 4 : 3;
+    geofsUiButton.insertBefore(generatePanelButtonHTML(), geofsUiButton.children[insertPos]);
 
     //remove original buttons
     const origButtons = document.getElementsByClassName('geofs-liveries geofs-list-collapsible-item');
@@ -38,14 +41,15 @@ let origHTMLs = {};
 /**
  * @param {Response} data
  */
-function handleLiveryJson(data) {
-    liveryobj = data.json();
+async function handleLiveryJson(data) {
+    liveryobj = await data.json();
 
     // mark aircraft with livery icons
     Object.keys(liveryobj.aircrafts).forEach(aircraftId => {
         const element = document.querySelector(`[data-aircraft='${aircraftId}']`);
-        if (!origHTMLs[aircraftId])
+        if (!origHTMLs[aircraftId]) {
             origHTMLs[aircraftId] = element.innerHTML;
+        }
 
         element.innerHTML = origHTMLs[aircraftId] +
             createTag('img', {src: `${githubRepo}/liveryselector-logo-small.svg`, height: '30px'}).outerHTML;
@@ -84,21 +88,21 @@ function loadLivery(texture, index, parts) {
  * Load liveries from text input fields
  */
 function inputLivery() {
-    let airplane = getCurrentAircraft();
-    let textures = airplane.liveries[0].texture;
-    let inputFields = document.getElementsByName('textureInput');
+    const airplane = getCurrentAircraft();
+    const textures = airplane.liveries[0].texture;
+    const inputFields = document.getElementsByName('textureInput');
     if (textures.filter(x => x === textures[0]).length === textures.length) { // the same texture is used for all indexes and parts
-        let texture = inputFields[0].value;
+        const texture = inputFields[0].value;
         loadLivery(Array(textures.length).fill(texture), airplane.index, airplane.parts);
     } else {
-        let texture = [];
+        const texture = [];
         inputFields.forEach(e => texture.push(e.value));
         loadLivery(texture, airplane.index, airplane.parts);
     }
 }
 
 function sortList(id) {
-    const list = document.getElementById(id);
+    const list = domById(id);
     let i, switching, b, shouldSwitch;
     switching = true;
     while (switching) {
@@ -122,20 +126,20 @@ function sortList(id) {
  * Generate main livery list
  */
 function listLiveries() {
-    document.getElementById('liverylist').innerHTML = '';
+    domById('liverylist').innerHTML = '';
 
-    let airplane = getCurrentAircraft();
+    const airplane = getCurrentAircraft();
     airplane.liveries.forEach(function (e) {
-        let listItem = appendChild(document.getElementById('liverylist'), 'li', {
+        let listItem = appendNewChild(domById('liverylist'), 'li', {
             id: [geofs.aircraft.instance.id, e.name, 'button'].join('_'),
-            onpointerenter: "this.style.background='#dedede'",
-            onpointerleave: "this.style.background='#ffffff'",
-            onclick: () => loadLivery(e.texture, airplane.index, airplane.parts)
+            onpointerenter: 'this.style.background=\'#dedede\'',
+            onpointerleave: 'this.style.background=\'#ffffff\''
         });
+        listItem.onclick = () => loadLivery(e.texture, airplane.index, airplane.parts);
         listItem.innerHTML = e.name;
-        listItem.style.display = "block";
+        listItem.style.display = 'block';
 
-        appendChild(listItem, 'span', {
+        appendNewChild(listItem, 'span', {
             id: [geofs.aircraft.instance.id, e.name].join('_'),
             class: 'fa fa-star nocheck',
             style: 'float: right; padding-top: 15px;',
@@ -152,18 +156,18 @@ function loadFavorites() {
     if (localStorage.getItem('favorites') === null) {
         localStorage.favorites = '';
     }
-    document.getElementById('favorites').innerHTML = '';
-    let list = localStorage.favorites.split(",");
-    let airplane = geofs.aircraft.instance.id;
+    domById('favorites').innerHTML = '';
+    const list = localStorage.favorites.split(',');
+    const airplane = geofs.aircraft.instance.id;
     list.forEach(function (e) {
         if ((airplane == e.slice(0, airplane.length)) && (e.charAt(airplane.length) == '_')) {
-            star(document.getElementById(e));
+            star(domById(e));
         }
     });
 }
 
 function addCustomForm() {
-    document.getElementById('customDiv').innerHTML = '';
+    domById('customDiv').innerHTML = '';
     const airplane = getCurrentAircraft();
     const textures = airplane.liveries[0].texture;
     const placeholders = airplane.labels;
@@ -175,10 +179,10 @@ function addCustomForm() {
 }
 
 function search(text) {
-    if (text === "") {
+    if (text === '') {
         listLiveries();
     } else {
-        var liveries = document.getElementById("liverylist").childNodes;
+        const liveries = domById('liverylist').childNodes;
         liveries.forEach(function (e) {
             const found = e.innerText.toLowerCase().includes(text.toLowerCase());
             e.style.display = found ? 'block' : 'none';
@@ -194,9 +198,9 @@ function search(text) {
 function star(element) {
     const e = element.classList;
     const elementId = [element.id, 'favorite'].join('_');
-    if (e == "fa fa-star nocheck") {
-        const btn = document.getElementById([element.id, 'button'].join('_'));
-        const fbtn = appendChild(document.getElementById('favorites'), 'li', {
+    if (e == 'fa fa-star nocheck') {
+        const btn = domById([element.id, 'button'].join('_'));
+        const fbtn = appendNewChild(domById('favorites'), 'li', {
             id: elementId,
             onclick: btn.getAttribute('onclick')
         });
@@ -207,56 +211,56 @@ function star(element) {
         list = [...new Set(list)];
         localStorage.favorites = list;
 
-    } else if (e == "fa fa-star checked") {
-        document.getElementById("favorites").removeChild(document.getElementById(elementId));
-        let list = localStorage.favorites.split(',');
-        let index = list.indexOf(element.id);
+    } else if (e == 'fa fa-star checked') {
+        domById('favorites').removeChild(domById(elementId));
+        const list = localStorage.favorites.split(',');
+        const index = list.indexOf(element.id);
         if (index !== -1) {
             list.splice(index, 1);
         }
         localStorage.favorites = list;
     }
     //style animation
-    e.toggle("checked");
-    e.toggle("nocheck");
+    e.toggle('checked');
+    e.toggle('nocheck');
 }
 
 /**
  * @param {string} id
  */
 function createUploadButton(id) {
-    const customDiv = document.getElementById('customDiv');
-    appendChild(customDiv, 'input', {
+    const customDiv = domById('customDiv');
+    appendNewChild(customDiv, 'input', {
         type: 'file',
         onchange: 'uploadLivery(this)',
         style: 'marginRight = -120px;'
     });
-    appendChild(customDiv, 'input', {
+    appendNewChild(customDiv, 'input', {
         type: 'text',
         name: 'textureInput',
         class: 'mdl-textfield__input address-input',
         placeholder: id,
         id: id
     });
-    appendChild(customDiv, 'br');
+    appendNewChild(customDiv, 'br');
 }
 
 function uploadLivery(e) {
-    var form = new FormData();
-    form.append("image", e.files[0]);
+    const form = new FormData();
+    form.append('image', e.files[0]);
 
-    var settings = {
-        "url": `https://api.imgbb.com/1/upload?key=${localStorage.imgbbAPIKEY}`,
-        "method": 'POST',
-        "timeout": 0,
-        "processData": false,
-        "mimeType": "multipart/form-data",
-        "contentType": false,
-        "data": form
+    const settings = {
+        'url': `https://api.imgbb.com/1/upload?key=${localStorage.imgbbAPIKEY}`,
+        'method': 'POST',
+        'timeout': 0,
+        'processData': false,
+        'mimeType': 'multipart/form-data',
+        'contentType': false,
+        'data': form
     };
 
     $.ajax(settings).done(function (response) {
-        var jx = JSON.parse(response);
+        const jx = JSON.parse(response);
         console.log(jx.data.url);
         e.nextSibling.value = jx.data.url;
     });
@@ -297,7 +301,7 @@ function createTag(name, attributes = {}) {
  * @param {number} pos insert in Nth position (default append)
  * @returns {HTMLElement}
  */
-function appendChild(parent, tagName, attributes = {}, pos=-1) {
+function appendNewChild(parent, tagName, attributes = {}, pos = -1) {
     const child = createTag(tagName, attributes);
     if (pos < 0) {
         parent.appendChild(child);
@@ -308,6 +312,14 @@ function appendChild(parent, tagName, attributes = {}, pos=-1) {
     return child;
 }
 
+/**
+ * @param {string} elementId
+ * @returns {HTMLElement}
+ */
+function domById(elementId) {
+    return document.getElementById(elementId);
+}
+
 /******************* HTML & CSS Templates *********************/
 
 /**
@@ -316,21 +328,21 @@ function appendChild(parent, tagName, attributes = {}, pos=-1) {
 function generateListHTML() {
     return `
         <h3><img src="${githubRepo}/liveryselector-logo.svg" width="95%" title="LiverySelector" style="display: block; margin-left: auto; margin-right: auto;"/></h3>
-        
+
         <div class="mdl-textfield mdl-js-textfield geofs-stopMousePropagation geofs-stopKeyupPropagation" style="width: 100%; padding-right: 86px;">
             <input class="mdl-textfield__input address-input" type="text" placeholder="Search liveries" onkeydown="search(this.value)" id="searchlivery">
             <label class="mdl-textfield__label" for="searchlivery">Search liveries</label>
         </div>
-        
+
         <h6>Favorite liveries</h6>
         <ul id="favorites" class="geofs-list geofs-visible"></ul>
-        
+
         <h6>Available liveries</h6>
         <ul id="liverylist" class=" geofs-list geofs-visible"></ul>
-        
+
         <h6 style="margin-bottom: -10px">Load external livery</h6>
         <div id="customDiv" class="mdl-textfield mdl-js-textfield geofs-stopMousePropagation geofs-stopKeyupPropagation" style="width: 100%; padding-right: 86px;"></div>
-        
+
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" onclick="inputLivery()">Load livery</button>
 `;
 }
@@ -338,25 +350,30 @@ function generateListHTML() {
 /**
  * @returns {HTMLElement} HTML template for main menu livery button
  */
-function generateButtonPanelHTML() {
-    const liveryButton = createTag('button', {title: 'Change livery',
+function generatePanelButtonHTML() {
+    const liveryButton = createTag('button', {
+        title: 'Change livery',
         id: 'liverybutton',
         class: 'mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly',
-        onclick: listLiveries(),
+        onclick: 'listLiveries()',
         'data-toggle-panel': '.geofs-livery-list',
         'data-tooltip-classname': 'mdl-tooltip--top',
-        'data-upgraded': ",MaterialButton"
+        'data-upgraded': ',MaterialButton'
     });
-    liveryButton.innerHTML = 'LIVERY' + createTag('img', {src: `${githubRepo}/liveryselector-logo-small.svg`, height: "30px"}).outerHTML;
+    liveryButton.innerHTML = 'LIVERY' + createTag(
+        'img',
+        {src: `${githubRepo}/liveryselector-logo-small.svg`, height: '30px'}
+    ).outerHTML;
 
     return liveryButton;
 }
 
 /**
- * @returns {string} HTML template
+ * @returns {HTMLElement} HTML template
  */
 function generateStylesHTML() {
-    return `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/><style>
+    const tag = createTag('style', {type: 'text/css'});
+    tag.innerHTML = `
         .checked {
             text-shadow: 0 0 1px black, 0 0 1px black, 0 0 1px black, 0 0 1px black;
             color: rgb(255, 193, 7);
@@ -364,7 +381,7 @@ function generateStylesHTML() {
             text align: right;
             cursor: pointer;
         }
-        
+
         .nocheck {
             text-shadow: 0 0 1px black, 0 0 1px black, 0 0 1px black, 0 0 1px black;
             color: white;
@@ -372,15 +389,15 @@ function generateStylesHTML() {
             text align: right;
             cursor: pointer;
         }
-        
+
         input[type="file"] {
             color: transparent;
         }
-        
+
         input[type="file"]::-webkit-file-upload-button {
             visibility: hidden;
         }
-        
+
         input[type="file"]::before {
             content: "UPLOAD IMAGE";
             display: inline-block;
@@ -401,7 +418,7 @@ function generateStylesHTML() {
             width: 120px;
             border-radius: 2px;
         }
-        
+
         input[name="textureInput"] {
             color: white;
             font-size: 14px;
@@ -414,10 +431,11 @@ function generateStylesHTML() {
             width: 65%;
             keyboard-events: none;
         }
-        
+
         input[name="textureInput"]::placeholder {
             color: color: rgb(252, 252, 252);
             font-size: 14px;
         }
-      </style>`;
+    `;
+    return tag;
 }
