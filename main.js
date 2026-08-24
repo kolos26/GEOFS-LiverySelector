05959db0364f5c5d33c9dfdf95b8f264017d20ff
Kolos26 <Kolos26>
2026-08-24 19:01:52 +0200

    #RELEASE 3.6.0

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const githubRepo = 'https://raw.githubusercontent.com/kolos26/GEOFS-LiverySelector/main';
 let jsDelivr = 'https://cdn.jsdelivr.net/gh/kolos26/GEOFS-LiverySelector@main';
 const noCommit = jsDelivr;
-const version = '3.5.1';
+const version = '3.6.0';
 
 const liveryobj = {};
 const mpLiveryIds = {};
